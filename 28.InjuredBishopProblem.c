05c9b6aa508f7b486d3ebf1d11f40230bc6af077
AshDyh1999 <759045809@qq.com>
2020-05-18 16:55:08 +0800

    基本完成

--- 28.InjuredBishopProblem.c
+++ 28.InjuredBishopProblem.c
@@ -4,13 +4,26 @@
 #include<math.h>
 int main(void){
     int result, start[2], end[2];
-    int t,N;
+    int t,N,count = 0;
     scanf("%d", &t);
     while(t--){
+    	count++;
         scanf("%d", &N);
         scanf("%d %d", &start[0], &start[1]);
         scanf("%d %d", &end[0], &end[1]);
-        
+    	if (abs(start[0] - end[0])%2 == abs(start[1] - end[1]) % 2)
+	    {
+	    	if (abs(start[0] - end[0]) <= abs(start[1] - end[1]))
+	    	{
+	    		result = abs(start[1] - end[1]);
+	    	}else{
+	    		result = abs(start[0] - end[0]);
+	    	}
+	    } else{
+	    	result = -1;
+	    }  
+	    if(count != 1)printf("\n");
+        printf("Case %d:\n%d\n",count, result);
     }
 
     return 0;
--- hello.c
+++ hello.c
@@ -1,26 +1,31 @@
-#include<iostream>
-#include <stdio.h>
-#include<map>
-using namespace std;
+#include<stdio.h>
+#include<stdlib.h>
+int in[1005],out[1005];
+int comp(const void*a,const void*b){
+    return *(int*)a-*(int*)b;
+}
 int main(){
-    int t,n,i,num,ans=0;
-    map<int,int> count;
-    scanf("%d", &t);
-    while(t--){
-        scanf("%d", &n);
-        count.clear();
-        ans=0;
-        for (i = 0; i < n; ++i)
-        {
-            scanf("%d", &num); 
-            count[num]++; 
+    int t,n,i,j;
+    scanf("%d",&t);
+    for(i=0;i<t;i++){
+        scanf("%d",&n);
+        for(j=0;j<n;j++){
+            scanf("%d",&in[j]);
         }
-        map<int,int>::iterator it=count.begin();
-        for(it=count.begin();it!=count.end();++it){
-            if(it->second>ans)
-            ans=it->second;
+        for(j=0;j<n;j++){
+            if(j==0)
+            out[0]=in[0];
+            else
+            out[j]=in[j]-in[j-1];
         }
-        printf("%d\n", ans);
+        qsort(out,n,sizeof(int),comp);
+        printf("Case %d:\n",i+1);
+        for(j=0;j<n;j++){
+            printf("%d",out[j]);
+            if(j!=n-1)
+            printf(" "); 
+        }
+        if(i!=t-1)
+        printf("\n\n");
     }
-    return 0;
 } 
